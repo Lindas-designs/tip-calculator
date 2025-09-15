ee85fe97ea3c9c78bf8ec93ae44dbed86bead6e9
Linda <kazusalinda@gmail.com>
2025-09-15 09:32:41 +0300

    extra save

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,1 +1,9 @@
-console.log(76);
+"use strict";
+const billInput = document.getElementById("input-bill");
+const resetBtn = document.querySelector(".btn-reset");
+
+resetBtn.addEventListener("click", function (e) {
+  const billValue = Number(billInput.value);
+  console.log(typeof billValue);
+  // Padomāt, kā pārbaudīt, ka tikai cipari, un nevis citi simboli ir input laukā. Ja nav tikai cipari, izdot erroru.
+});
