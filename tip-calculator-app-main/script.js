25af922a84b2fb25ffb79450ffcd5f8db17edcd1
Linda <kazusalinda@gmail.com>
2025-09-15 15:57:44 +0300

    javascript init

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,9 +1,29 @@
 "use strict";
 const billInput = document.getElementById("input-bill");
 const resetBtn = document.querySelector(".btn-reset");
+const errMessageBill = document.querySelector(".err-bill");
+const errMessagePeople = document.querySelector(".err-people");
+const buttons = document.querySelectorAll(".btn-percent");
+
+buttons.forEach((button) =>
+  button.addEventListener("click", function (e) {
+    buttons.forEach((button) => button.classList.remove("active"));
+    button.classList.add("active");
+  })
+);
+
+const regex = /^\d+(\.\d+)?$/;
+function testBillInput(variable) {
+  return regex.test(variable);
+}
 
 resetBtn.addEventListener("click", function (e) {
-  const billValue = Number(billInput.value);
-  console.log(typeof billValue);
-  // Padomāt, kā pārbaudīt, ka tikai cipari, un nevis citi simboli ir input laukā. Ja nav tikai cipari, izdot erroru.
+  const billValue = billInput.value;
+  let billIsTrue;
+  testBillInput(billValue) ? (billIsTrue = true) : (billIsTrue = false);
+  if (!billIsTrue) {
+    errMessageBill.classList.remove("closed");
+  } else if (billIsTrue) {
+    errMessageBill.classList.add("closed");
+  }
 });
